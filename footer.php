a9dea6d9ef93a1771ce7ad30b9d9f4354b11dde2
Felix Blanco <felixablancoe@gmail.com>
2017-09-05 11:57:06 -0400

    Integracion de las imagenes, agregamos el header y el footer

---
+++ footer.php
@@ -0,0 +1,33 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<p class="text-justify">
+					Canamelar esta formada por toda una familia descendiente de Evariste manteniendo la cultura y desarrollo, estamos ubicados en plena Avenida después de la Estatua de Los Indios Chaimas.
+				</p>
+			</div>
+			<div class="col-md-4 text-center">
+				<h5><span class="icon-mobile"></span> Yisaida Evariste 0424-90-889-51</h5>
+				<h5><span class="icon-envelope"></span> canamelar@gmail.com</h5>
+
+				<ul class="list-inline">
+					<li><a href="https://www.facebook.com/canamelar.evariste" target="_blank"><img src=" <?php bloginfo('template_url'); ?>/img/r_s/facebook.png" alt="facebook de canamelar" class="img-responsive center-block" width="40"> </a></li>
+					<li><a href="https://twitter.com/CanamelarE" target="_blank"><img src=" <?php bloginfo('template_url'); ?>/img/r_s/twitter.png" alt="Twitter de Canamelar" class="img-responsive center-block" width="40"> </a></li>
+					<li><a href="https://plus.google.com/u/0/108337097178712335850/posts" target="_blank"><img src=" <?php bloginfo('template_url'); ?>/img/r_s/google-plus.png" alt="Google Plus de Canamelar" class="img-responsive center-block" width="40"> </a></li>
+					<li><a href="http://www.youtube.com/channel/UCIY1ZNYuRzgBXuNabfSUAjg" target="_blank"><img src=" <?php bloginfo('template_url'); ?>/img/r_s/youtube.png" alt="Youtube de Canamelar" class="img-responsive center-block" width="40"> </a></li>
+				</ul>
+			</div>
+			<div class="col-md-4">
+				<a href="http://felixblanco.com.ve/" target="_blank">
+					<img src="<?php bloginfo('template_url') ?>/img/logoFB.png" alt="" class="img-responsive center-block" width="160">
+				</a>
+			</div>
+		</div>
+	</div>
+</footer>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+
+	<title><?php bloginfo('name');  ?></title>
+
+	<!-- Personalizacion de los iconos que vamos a utilizar -->
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/ico/style.css">
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Style del tema -->
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+	
+	<!-- Jquery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+</head>
+<body>
+
+<header>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-6">
+				<h1>Canamelar</h1>
+			</div>
+			<div class="col-md-6">
+				<nav class="text-center">
+					<ul class="list-inline">
+						<li><a href="http://localhost/wordpress/index.php/paginaejemplo2/">Baile del Mono</a></li>
+						<li><a href="http://localhost/wordpress/index.php/somo-canamelar/">Canamelar</a></li>
+						<li><a href="http://localhost/wordpress/index.php/paginaejemplo1/">Historias</a></li>
+						<li><a href="#">Presentaciones</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</div>
+</header>
