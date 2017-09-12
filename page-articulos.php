cc54686fb426624a045eb498a445b3ddd5103acc
Felix Blanco <felixablancoe@gmail.com>
2017-09-12 07:52:12 -0400

    agregando la el modelo de la pagina de articulos

--- page-articulos.php
+++ page-articulos.php
@@ -1,25 +1,25 @@
 <?php /* Template Name: Articulos */ define ('WP_USE_THEMES', false); get_header();?>
 
-<div class="container MarginTop2 LoopArticulo">
+<div class="container MarginTop2">
 	<div class="row">
-		<?php 
-			query_posts(array('posts_per_page'=>10 )); 
-			if ( have_posts() ) : while (have_posts()) : the_post(); ?>
-			<div class="col-md-4">
-				<article class="PocoPadding">
-					<a href="<?php the_permalink(); ?>">
-						<?php 
-							the_post_thumbnail('',['class'=>'img-responsive']); 
-							the_title("<h2>","</h2>");
-						?>
-					</a>
-					<div class="row">
-						<div class="col-md-5"><?php the_time('d M Y'); ?></div>
-						<div class="col-md-6"><?php the_category(','); ?></div>
-					</div>
-				</article>
-			</div>
-		<?php endwhile; endif; ?> 
+		<div class="col-md-7 TextBlack">
+			<a href="<?php the_permalink(); ?>">
+				<?php query_posts('posts_per_page=10' );
+					if( have_posts() ):while( have_posts() ): the_post();
+						echo '<article class="MarginTop2">';
+						the_post_thumbnail('',['class'=>'img-responsive center-block'] );
+						the_title('<h3>','</h3>');
+						the_excerpt();
+						echo "<span class='glyphicon glyphicon-calendar'> </span>";
+						the_date('d M Y');
+						echo "<span class='glyphicon glyphicon-tag'></span>";
+						the_category(',');
+						echo '</article>';
+					endwhile; endif;
+				?>
+			</a>
+		</div>
+		<?php get_sidebar(); ?>
 	</div>
 </div>
 	
